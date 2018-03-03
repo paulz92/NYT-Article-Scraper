6f8332318c01e2e5f9c1f186dd6094dac4398ee1
Paul Zavattieri <paulz92@gmail.com>
2018-03-02 22:54:27 -0500

    fix

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ mongoose.connect(
   }
 );
 
+console.log(process.env.NYT_API_KEY);
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
